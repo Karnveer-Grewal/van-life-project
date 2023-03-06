dca71b184db7a01f218495044e225d8d247e29d3
Karnveer Grewal <karnveergrewal4@gmail.com>
2023-03-05 22:35:20 -0500

    added routes and displayed info w/ miragejs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import React from 'react';
+import About from './components/About';
+import Home from './components/Home';
+import Vans from './components/Vans';
+import VanDetail from './components/VanDetail';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 function App() {
-  return <div className='App'>Van Life Application</div>;
+  return (
+    <Router>
+      <header>
+        <Link to='/'>#VANLIFE</Link>
+        <nav>
+          <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Vans.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../server';
+
+const VansList = () => {
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('/api/vans');
+      const data = await response.json();
+      console.log(data);
+      setVans(data.vans);
+    }
+    getData();
+  }, []);
+
+  console.log(vans);
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id}>
+      <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} alt={van.name} width='100px' />
+        <div>
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i>{van.type}</i>
+      </Link>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Explore our van options</h1>
+      {vanElements}
+    </div>
+  );
+};
+
+export default VansList;
