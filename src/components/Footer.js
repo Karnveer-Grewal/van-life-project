01f42a269121c7741421d20911cfcbca8e3fa05d
Karnveer Grewal <karnveergrewal4@gmail.com>
2023-03-06 12:45:07 -0500

    adjusted styling to navs for different routes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer--container'>
-      <small>Vanlife &copy; 2023</small>
+      <small>&copy; #VANLIFE 2023</small>
     </footer>
   );
 };
